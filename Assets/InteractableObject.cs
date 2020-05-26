506addd5ccda9a09ef0f200099654cc8b8b82f90
4happy-studio <4happy.studio@gmail.com>
2020-05-26 20:17:13 +0700

    Initial commit

---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject textUI;
+    public UnityEvent m_MyEvent;
+
+    private void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        textUI.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        textUI.SetActive(false);
+    }
+}
